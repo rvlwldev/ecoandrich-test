cfb00bdefaabebb1af24fd98c9b40d2102fa652c
rvlwldev <rvlwlasd77@gmail.com>
2023-05-15 13:49:08 +0900

    project build

---
+++ src/main/java/com/ecoandrich/maycodingTest/MayCodingTestApplication.java
@@ -0,0 +1,13 @@
+package com.ecoandrich.maycodingTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayCodingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayCodingTestApplication.class, args);
+	}
+
+}
