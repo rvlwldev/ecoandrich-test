45a0641bcde56bb18e4e389b33c735df79e74722
rvlwldev <rvlwlasd77@gmail.com>
2023-05-17 17:07:04 +0900

    feat/refactor: Employee의 Job 예외 처리 추가 구현 및 코드 리팩토링

--- src/main/java/com/ecoandrich/maycodingTest/HR/_Common/ExceptionHandler/Exception/JobHistoryNotFoundException.java
+++ src/main/java/com/ecoandrich/maycodingTest/HR/_Common/ExceptionHandler/Exception/JobHistoryNotFoundException.java
@@ -1,10 +1,10 @@
 package com.ecoandrich.maycodingTest.HR._Common.ExceptionHandler.Exception;
 
-import static com.ecoandrich.maycodingTest.HR._Common.ExceptionHandler.MessageEnum.EmployeeStatus.NOT_FOUND_JOB_HISTORY_FORMAT;
+import static com.ecoandrich.maycodingTest.HR._Common.ExceptionHandler.MessageEnum.EmployeeStatus.JOB_HISTORY_NOT_FOUND_FORMAT;
 
 public class JobHistoryNotFoundException extends RuntimeException {
 
     public JobHistoryNotFoundException(long id) {
-        super(NOT_FOUND_JOB_HISTORY_FORMAT.toString(id));
+        super(JOB_HISTORY_NOT_FOUND_FORMAT.toString(id));
     }
 }
--- src/main/java/com/ecoandrich/maycodingTest/HR/_Common/ExceptionHandler/HRExceptionHandler.java
+++ src/main/java/com/ecoandrich/maycodingTest/HR/_Common/ExceptionHandler/HRExceptionHandler.java
@@ -2,6 +2,7 @@ package com.ecoandrich.maycodingTest.HR._Common.ExceptionHandler;
 
 import com.ecoandrich.maycodingTest.HR._Common.ExceptionHandler.Exception.EmployeeNotFoundException;
 import com.ecoandrich.maycodingTest.HR._Common.ExceptionHandler.Exception.JobHistoryNotFoundException;
+import com.ecoandrich.maycodingTest.HR._Common.ExceptionHandler.Exception.JobNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,19 +16,34 @@ public class HRExceptionHandler {
 
     @ExceptionHandler(EmployeeNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleOfEmployeeException(EmployeeNotFoundException e) {
-        return getResponseBodyMap("error", e.getMessage(), HttpStatus.BAD_REQUEST);
+        return getResponseEntityWithBody("error", e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(JobHistoryNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleOfEmployeeException(JobHistoryNotFoundException e) {
-        return getResponseBodyMap("error", e.getMessage(), HttpStatus.NOT_FOUND);
+        return getResponseEntityWithBody("error", e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    private ResponseEntity<Map<String, Object>> getResponseBodyMap(String key, Object value, HttpStatus status) {
+    @ExceptionHandler(JobNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleOfEmployeeException(JobNotFoundException e) {
+        HashMap<String, Object> body = new HashMap<>() {{
+            put("error", e.getMessage());
+            put("inputJobId", e.getInvalidJobId());
+        }};
+
+        return getResponseEntityWithBody(body, HttpStatus.NOT_FOUND);
+    }
+
+
+    private ResponseEntity<Map<String, Object>> getResponseEntityWithBody(String key, Object value, HttpStatus status) {
         HashMap<String, Object> body = new HashMap<>() {{
             put(key, value);
         }};
 
+        return ResponseEntity.status(status).body(body);
+    }
+
+    private ResponseEntity<Map<String, Object>> getResponseEntityWithBody(Map<String, Object> body, HttpStatus status) {
         return ResponseEntity.status(status).body(body);
     }
 
