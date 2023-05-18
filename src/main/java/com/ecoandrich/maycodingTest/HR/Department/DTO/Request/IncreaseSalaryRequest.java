f81da46bbdad2bf9e49a5649ad3981bc0a6c1772
rvlwldev <rvlwlasd77@gmail.com>
2023-05-18 22:42:09 +0900

    docs: HR 도메인 Request DTO 문서화 적용

--- src/main/java/com/ecoandrich/maycodingTest/HR/Department/DTO/Request/IncreaseSalaryRequest.java
+++ src/main/java/com/ecoandrich/maycodingTest/HR/Department/DTO/Request/IncreaseSalaryRequest.java
@@ -1,8 +1,12 @@
 package com.ecoandrich.maycodingTest.HR.Department.DTO.Request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+@ApiModel(value = "부서 급여 인상 요청 양식")
 @Data
 public class IncreaseSalaryRequest {
+    @ApiModelProperty(value = "인상 비율", example = "15.5% 경우, 0.155 또는 15.5")
     private double increasePercent;
 }
--- src/main/java/com/ecoandrich/maycodingTest/HR/Employee/DTO/Request/EmployeeRequest.java
+++ src/main/java/com/ecoandrich/maycodingTest/HR/Employee/DTO/Request/EmployeeRequest.java
@@ -1,19 +1,42 @@
 package com.ecoandrich.maycodingTest.HR.Employee.DTO.Request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.LocalDate;
 
+@ApiModel(value = "사원 정보 수정 요청 양식", description = "사원 정보 수정 Request DTO")
 @Data
 public class EmployeeRequest {
+
+    @ApiModelProperty(value = "이름")
     private String firstName;
+
+    @ApiModelProperty(value = "성")
     private String lastName;
+
+    @ApiModelProperty(value = "이메일")
     private String email;
+
+    @ApiModelProperty(value = "휴대폰번호", example = "515.127.1234")
     private String phoneNumber;
+
+    @ApiModelProperty(value = "고용일", example = "2023-05-17")
     private LocalDate hireDate;
+
+    @ApiModelProperty(value = "직무 번호")
     private String jobId;
+
+    @ApiModelProperty(value = "급여")
     private Double salary;
+
+    @ApiModelProperty(value = "커미션 퍼센트")
     private Double commissionPct;
+
+    @ApiModelProperty(value = "매니저 번호")
     private Long managerId;
+
+    @ApiModelProperty(value = "부서 번호")
     private Long departmentId;
 }
