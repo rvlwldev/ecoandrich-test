ec4aa9f204286e396bf3374611e2ee9ccaf08a5f
rvlwldev <rvlwlasd77@gmail.com>
2023-05-18 23:00:02 +0900

    docs: HR/Department 도메인 Response DTO 문서화 적용

--- src/main/java/com/ecoandrich/maycodingTest/HR/Department/DTO/Response/Department/DepartmentNameListResponse.java
+++ src/main/java/com/ecoandrich/maycodingTest/HR/Department/DTO/Response/Department/DepartmentNameListResponse.java
@@ -1,15 +1,20 @@
 package com.ecoandrich.maycodingTest.HR.Department.DTO.Response.Department;
 
 import com.ecoandrich.maycodingTest.HR.Department.Entity.Department;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@ApiModel(value = "해당 위치 부서명 목록", description = "위치 조회 시 해당 위치의 부서명 목록")
 @Data
 @Builder
 public class DepartmentNameListResponse {
+
+    @ApiModelProperty(value = "부서명 목록")
     private List<String> names;
 
     public static DepartmentNameListResponse toResponse(List<Department> department) {
--- src/main/java/com/ecoandrich/maycodingTest/HR/Department/DTO/Response/Location/LocationWithDepartmentNamesResponse.java
+++ src/main/java/com/ecoandrich/maycodingTest/HR/Department/DTO/Response/Location/LocationWithDepartmentNamesResponse.java
@@ -2,12 +2,15 @@ package com.ecoandrich.maycodingTest.HR.Department.DTO.Response.Location;
 
 import com.ecoandrich.maycodingTest.HR.Department.DTO.Response.Department.DepartmentNameListResponse;
 import com.ecoandrich.maycodingTest.HR.Department.SubEntity.Location;
+import io.swagger.annotations.ApiModel;
 import lombok.Builder;
 import lombok.Data;
 
+@ApiModel(value = "위치별 부서명 조회 결과")
 @Data
 @Builder
 public class LocationWithDepartmentNamesResponse {
+
     private LocationResponse location;
     DepartmentNameListResponse departments;
 
