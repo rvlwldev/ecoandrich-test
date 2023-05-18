13d1b163c223550e138b693e9bc20b447f7b9c57
rvlwldev <rvlwlasd77@gmail.com>
2023-05-18 23:14:32 +0900

    docs: 외부 API 문서화 적용

--- src/main/java/com/ecoandrich/maycodingTest/API/CoinAPI/DTO/Response/CryptoDetailResponse.java
+++ src/main/java/com/ecoandrich/maycodingTest/API/CoinAPI/DTO/Response/CryptoDetailResponse.java
@@ -1,12 +1,23 @@
 package com.ecoandrich.maycodingTest.API.CoinAPI.DTO.Response;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+@ApiModel(value = "코인 가격 정보")
 @Data
 public class CryptoDetailResponse {
+
+    @ApiModelProperty(value = "시가")
     Double openingPrice;
+
+    @ApiModelProperty(value = "고가")
     Double highPrice;
+
+    @ApiModelProperty(value = "저가")
     Double lowPrice;
+
+    @ApiModelProperty(value = "변동률")
     Double signedChangeRate;
 
     public void setSignedChangeRate(Double signedChangeRate) {
